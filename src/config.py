9a27ad58cf7e954847a3879c5b04d05a204f7949
Emmanuel <xtrememanos@windowslive.com>
2019-12-24 16:07:56 +1100

    Added empty config file

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# Constant table
+user = '' # Twitch user id
+pwd  = '' # not needed currently
+host ='localhost'
+db   = '' # not needed currently
+oath = '' # twitch auth token, get here https://twitchapps.com/tmi/
+clientid= '' # twitch client id, get here  https://dev.twitch.tv/console/apps/create
+botlist = ['admiralbullbot', user] # list of bots in channel, user is added here for testing
