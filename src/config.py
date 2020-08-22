8b0b215cce914f5df52fe2f1375f06e5c5849d12
Emmanuel <xtrememanos@windowslive.com>
2020-08-22 13:02:26 +1000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ user = 'sikzone' # Twitch user id
 pwd  = '' # not needed currently
 host ='localhost'
 db   = '' # not needed currently
-oath = 'oauth:9elmmo1x8ptst5nglx9s9rxv3etfz8' # twitch auth token, get here https://twitchapps.com/tmi/
+oath = '9elmmo1x8ptst5nglx9s9rxv3etfz8' # twitch auth token, get here https://twitchapps.com/tmi/
 clientid= 'ijp0enoedm9be3m7thuiqe2nitzvwt' # twitch client id, get here  https://dev.twitch.tv/console/apps/create
 botlist = ['admiralbullbot', user] # list of bots in channel, user is added here for testing
